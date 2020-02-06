4ff38052ecfc78601adc3ce81dbf69641faaba04
rajarampakur <pakurrajaram@gmail.com>
2020-02-06 07:42:51 +0545

    Updates

---
+++ precache-manifest.25eca1ba09b93a9a3dbbfbe0501ba15d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1142b0045968f1b434b",
+    "url": "/react-with-redux-sample-project/static/css/main.ed746ece.chunk.css"
+  },
+  {
+    "revision": "d1142b0045968f1b434b",
+    "url": "/react-with-redux-sample-project/static/js/main.484ddced.chunk.js"
+  },
+  {
+    "revision": "9b8bb816b817d1c06b3b",
+    "url": "/react-with-redux-sample-project/static/js/runtime~main.5f4a4482.js"
+  },
+  {
+    "revision": "08b6bde7239973d1670f",
+    "url": "/react-with-redux-sample-project/static/js/2.9b976a72.chunk.js"
+  },
+  {
+    "revision": "d17f686db71ec34d352f",
+    "url": "/react-with-redux-sample-project/static/js/3.30aa7bd4.chunk.js"
+  },
+  {
+    "revision": "570ad65385bb7ea373b7",
+    "url": "/react-with-redux-sample-project/static/js/4.b7f96ece.chunk.js"
+  },
+  {
+    "revision": "631433b254822979ea6a",
+    "url": "/react-with-redux-sample-project/static/js/5.38ec8ca4.chunk.js"
+  },
+  {
+    "revision": "8ec8cf3d29ae0a905201",
+    "url": "/react-with-redux-sample-project/static/js/6.3b92da29.chunk.js"
+  },
+  {
+    "revision": "2ee94195fd8d55c3e8e1",
+    "url": "/react-with-redux-sample-project/static/js/7.b68e7c00.chunk.js"
+  },
+  {
+    "revision": "99a5390511f62bdcbbc1",
+    "url": "/react-with-redux-sample-project/static/js/8.291fc08d.chunk.js"
+  },
+  {
+    "revision": "2e4ad848f5cbaa1f925e",
+    "url": "/react-with-redux-sample-project/static/js/9.366c7e75.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-with-redux-sample-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-with-redux-sample-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-with-redux-sample-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-with-redux-sample-project/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/react-with-redux-sample-project/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "a23c0ba21d92e0cd504f4115865c3355",
+    "url": "/react-with-redux-sample-project/static/media/1.a23c0ba2.jpg"
+  },
+  {
+    "revision": "2f98341f60de699f6cea9e0476417be0",
+    "url": "/react-with-redux-sample-project/static/media/10.2f98341f.jpg"
+  },
+  {
+    "revision": "2204d5c5da7c955c6d909704f79fbcd4",
+    "url": "/react-with-redux-sample-project/static/media/11.2204d5c5.jpg"
+  },
+  {
+    "revision": "3ec3f93d8db881ddf1b0757fa98fd5cb",
+    "url": "/react-with-redux-sample-project/static/media/12.3ec3f93d.jpg"
+  },
+  {
+    "revision": "4d9c0161a35f6867743142aecd2ce733",
+    "url": "/react-with-redux-sample-project/static/media/2.4d9c0161.jpg"
+  },
+  {
+    "revision": "6aa8cbb09a2c79d0c2daf64aba3fb57e",
+    "url": "/react-with-redux-sample-project/static/media/3.6aa8cbb0.jpg"
+  },
+  {
+    "revision": "241e6edf38311b287b115ede497e2cf1",
+    "url": "/react-with-redux-sample-project/static/media/4.241e6edf.jpg"
+  },
+  {
+    "revision": "c5fb808408fe27726ff479a666b9b369",
+    "url": "/react-with-redux-sample-project/static/media/5.c5fb8084.jpg"
+  },
+  {
+    "revision": "618b3de23a59692c96fd7c2989df599b",
+    "url": "/react-with-redux-sample-project/static/media/6.618b3de2.jpg"
+  },
+  {
+    "revision": "c81dd9c01f1b313b1c47c378448f9eb0",
+    "url": "/react-with-redux-sample-project/static/media/7.c81dd9c0.jpg"
+  },
+  {
+    "revision": "99d2c7eb6d69e7f259187c9b9c20c3b9",
+    "url": "/react-with-redux-sample-project/static/media/8.99d2c7eb.jpg"
+  },
+  {
+    "revision": "e7ff62401a724b217716a2bfe69aed30",
+    "url": "/react-with-redux-sample-project/static/media/9.e7ff6240.jpg"
+  },
+  {
+    "revision": "ca750497929e03cc788694ee2257b93d",
+    "url": "/react-with-redux-sample-project/static/media/blog-regular.ca750497.jpg"
+  },
+  {
+    "revision": "8b45a5b4a1c52eeb00b58bae74cb0b0b",
+    "url": "/react-with-redux-sample-project/static/media/single-blog.8b45a5b4.jpg"
+  },
+  {
+    "revision": "ec4c49daffa757ac6c9926f87590713c",
+    "url": "/react-with-redux-sample-project/static/media/single-portfolio.ec4c49da.png"
+  },
+  {
+    "revision": "09ee9ab69f490acc12b970935960d54f",
+    "url": "/react-with-redux-sample-project/index.html"
+  }
+];
